6a0f8571f5983fad51b6842638d69635206a3bd8
Ronny Deng <ronnydeng67@gmail.com>
2022-10-31 23:35:11 -0400

    two cars pics are able to rende

--- src/scripts/info.js
+++ src/scripts/info.js
@@ -1,14 +1,21 @@
 
 
-function renderInfo(carData) {
+function renderInfo(car1, car2) {
     // console.log(carData);
     // debugger
-    // document.getElementById("car_brand").innerHTML = carData.brand;
-    // document.getElementById("car_model").innerHTML = carData.model;
-    // document.getElementById("car_hp").innerHTML = carData.hp;
-    // document.getElementById("car_fuel").innerHTML = carData.fuel;
-    // document.getElementById("car_price").innerHTML = carData.price;
-    // document.getElementById("car_seat").innerHTML = carData.capacity;
+    document.getElementByClassName("brand1").innerHTML = car1.brand;
+    document.getElementByClassName("model1").innerHTML = car1.model;
+    document.getElementByClassName("hp1").innerHTML = car1.hp;
+    document.getElementByClassName("fuel1").innerHTML = car1.fuel;
+    document.getElementByClassName("price1").innerHTML = car1.price;
+    document.getElementByClassName("seats1").innerHTML = car1.capacity;
+
+    document.getElementByClassName("brand2").innerHTML = car2.brand;
+    document.getElementByClassName("model2").innerHTML = car2.model;
+    document.getElementByClassName("hp2").innerHTML = car2.hp;
+    document.getElementByClassName("fuel2").innerHTML = car2.fuel;
+    document.getElementByClassName("price2").innerHTML = car2.price;
+    document.getElementByClassName("seats2").innerHTML = car2.capacity;
 }
 
 module.exports = renderInfo;
--- src/scripts/search.js
+++ src/scripts/search.js
@@ -8,78 +8,62 @@ function searchJson(userData, jsonData) {
     function findMatch() {
 
         let res = [];
-        
-        
 
-        // for (let i = 0; i < data.length; i++) {
-        //     let obj = data[i];
+        let closeMatchCar = findCloseMatch(inputData, data);
+        console.log(closeMatchCar)
+        res.push(closeMatchCar[0])
+        res.push(closeMatchCar[1])
+        document.getElementById("found_result").innerHTML = "Your closest matches are:"
 
-        //     if (
-        //         hpRange(inputData.hp, obj.hp) &&
-        //         fuelRange(inputData.fuel, obj.fuel) && 
-        //         priceRange(inputData    .price, obj.price) &&
-        //         (inputData.capacity == obj.capacity) &&
-        //         (inputData.body === obj.body) &&
-        //         (inputData.transmission === obj.transmission) &&
-        //         (inputData.awd === obj.awd) &&
-        //         (inputData.ev === obj.ev)
-        //         ) {
-        //         res.push(obj);
-        //     }
-        // }
-        
-        // if (res.length === 0) {
-        //     document.getElementById("brand1").innerHTML = "Sorry, No cars found!!"
-            
-        // }
-
-
-        // if (res.length < 2) {
-            let closeMatchCar = findCloseMatch(inputData, data);
-            console.log(closeMatchCar)
-            res.push(closeMatchCar[0])
-            document.getElementById("found_result").innerHTML = "Sorry, I can't find a exact match, but here is your close match:"
-            // res.push(closeMatchCar[1])
-        // } 
-        // else {
-        //     document.getElementById("found_result").innerHTML = "Your match woule be";
-        // }
-        // else if (res.length === 1) {
-        //     let closeMatchCar = findCloseMatch(inputData, data);
-        //     res.push(closeMatchCar[1])
-        //     console.log(res)
-        // } 
         
             
-            document.getElementById("brand1").innerHTML = res[0].brand;
-            document.getElementById("model1").innerHTML = res[0].model;
+        // document.getElementByClassName("brand1").innerHTML = res[0].brand;
+        // document.getElementByClassName("model1").innerHTML = res[0].model;
+        // document.getElementByClassName("hp1").innerHTML = res[0].hp;
+        // document.getElementByClassName("fuel1").innerHTML = res[0].fuel;
+        // document.getElementByClassName("seats1").innerHTML = res[0].capacity;
 
-            let keyWord = res[0].brand + " " + res[0].model;
-            // console.log(keyWord)
-            let accessKey = "nJQFxVnXFwVju-EBYAE1jndUUt1suJC9BMs1iPg_Zl8";
-            let endpoint = `https://api.unsplash.com/search/photos?query=${keyWord}&client_id=${accessKey}&per_page=3&orientation=landscape`
-            let acutalImage = document.querySelector("#unsplash_imge1");
-            let imageLink = document.querySelector("#image_link1");
-            let acutalImage2 = document.querySelector("#unsplash_imge2");
-            let imageLink2 = document.querySelector("#image_link2");
-            let acutalImage3 = document.querySelector("#unsplash_imge3");
-            let imageLink3 = document.querySelector("#image_link3");
-    
-            fetch(endpoint)
-            .then((response) => {
-                return response.json()
-            })
-            .then((unsplashImage) => {
+        let keyWord = res[0].brand + " " + res[0].model;
+        let accessKey = "nJQFxVnXFwVju-EBYAE1jndUUt1suJC9BMs1iPg_Zl8";
+        let endpoint = `https://api.unsplash.com/search/photos?query=${keyWord}&client_id=${accessKey}&per_page=3&orientation=landscape`
+        let acutalImage = document.querySelector("#unsplash_imge1");
+        let imageLink = document.querySelector("#image_link1");
+        let acutalImage2 = document.querySelector("#unsplash_imge2");
+        let imageLink2 = document.querySelector("#image_link2");
 
-                acutalImage.src = unsplashImage.results[0].urls.small;
-                imageLink.setAttribute("href", unsplashImage.results[0].links.html);
-                acutalImage2.src = unsplashImage.results[1].urls.small;
-                imageLink2.setAttribute("href", unsplashImage.results[1].links.html);
-                // acutalImage3.src = unsplashImage.results[2].urls.small;
-                // imageLink3.setAttribute("href", unsplashImage.results[2].links.html);
-            })
+        fetch(endpoint)
+        .then((response) => {
+            return response.json()
+        })
+        .then((unsplashImage) => {
 
-            renderInfo(res[0]);
+            acutalImage.src = unsplashImage.results[0].urls.small;
+            imageLink.setAttribute("href", unsplashImage.results[0].links.html);
+            acutalImage2.src = unsplashImage.results[1].urls.small;
+            imageLink2.setAttribute("href", unsplashImage.results[1].links.html);
+        })
+
+        let keyWord1 = res[1].brand + " " + res[1].model;
+        // let accessKey = "nJQFxVnXFwVju-EBYAE1jndUUt1suJC9BMs1iPg_Zl8";
+        let endpoint2 = `https://api.unsplash.com/search/photos?query=${keyWord1}&client_id=${accessKey}&per_page=3&orientation=landscape`
+        let acutalImage_2 = document.querySelector("#unsplash1_imge1");
+        let imageLink_2 = document.querySelector("#image1_link1");
+        let acutalImage2_2 = document.querySelector("#unsplash1_imge2");
+        let imageLink2_2 = document.querySelector("#image1_link2");
+
+        fetch(endpoint2)
+        .then((response) => {
+            return response.json()
+        })
+        .then((unsplashImage1) => {
+
+            acutalImage_2.src = unsplashImage1.results[0].urls.small;
+            imageLink_2.setAttribute("href", unsplashImage1.results[0].links.html);
+            acutalImage2_2.src = unsplashImage1.results[1].urls.small;
+            imageLink2_2.setAttribute("href", unsplashImage1.results[1].links.html);
+        })
+
+        renderInfo(res[0], res[1]);
         
 
         
